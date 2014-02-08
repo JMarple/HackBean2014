d1c52e2cc9f0308d6b8e82be1cb4060d60bf60e1
Marc Rudkowski <coolrudski@gmail.com>
2014-02-07 23:32:35 -0500

    Test HTML

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
-<?php 
+<?php
 
-echo "Hackbean Test";
+<<<<<<< HEAD
+?> 
+    
+    
 
+
+<html>
+    
+    
+    
+   <p> Test Text </p> 
+    
+    
+    
+    
+    
+</html>
+    
+=======
+	$host = "us-cdbr-east-05.cleardb.net";
+	$user = "b85ad415edfa4d";
+	$pass = "df62fd56";
+	$db = "hackbean";
+	
+	$mysqli = new mysqli($host, $user, $pass, $db);
+
+	echo "Database Connection Test";
 ?>
+>>>>>>> 8ab41e7101151c7831321537dcbb348d2c38fb2c
