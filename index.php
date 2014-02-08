1411a8ece27664b1bd01fd0713b1d75c31794400
Justin Marple <Jmarple@umass.edu>
2014-02-07 23:33:16 -0500

    database test 2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+	
 	$host = "us-cdbr-east-05.cleardb.net";
 	$user = "b85ad415edfa4d";
 	$pass = "df62fd56";
@@ -7,5 +7,8 @@
 	
 	$mysqli = new mysqli($host, $user, $pass, $db);
 
-	echo "Database Connection Test";
+	if ($mysqli->connect_errno) {
+  	  echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+	echo $mysqli->host_info . "\n";
 ?>
