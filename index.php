d4da9faf9a6d26f76cda1c474d8bd06bd1bdfdc4
Justin Marple <Jmarple@umass.edu>
2014-02-08 00:00:59 -0500

    again a test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	
     echo "test";
     
-	//$host = "localhost";
+	/*//$host = "localhost";
 	$host = "us-cdbr-east-05.cleardb.net";
 	//$user = "root";
 	$user = "b85ad415edfa4d";
@@ -22,10 +22,23 @@
 		mysqli_query($mysqli, "INSERT INTO users (username, password) VALUES ('Justin', 'test')");
 	}
 	
-	echo $mysqli->host_info . "\n";
+	echo $mysqli->host_info . "\n";*/
+    
+    //$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+    
+    //$server = "localhost"; 
+    $server = "us-cdbr-east-05.cleardb.net";
+    //$username = "root"; 
+    $username = "b85ad415edfa4d";
+    //$password = ""; 
+    $password = "df62fd56";
+    $db = "hackbean";
+    
+    mysql_connect($server, $username, $password);
 	
-	
-	
+    mysql_select_db($db);
+    
+    mysql_query("INSERT INTO users (username, password) VALUES ('Justin', 'test')");
 	
 ?>
 
