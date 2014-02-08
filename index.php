2da86c23b8eb0baa86e6829c15c4a6da07837113
Justin Marple <Jmarple@umass.edu>
2014-02-07 23:42:40 -0500

    another database test please work

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <?php 
 	
+    echo "test";
+    
+	//$host = "localhost";
 	$host = "us-cdbr-east-05.cleardb.net";
+	//$user = "root";
 	$user = "b85ad415edfa4d";
+	//$pass = "";
 	$pass = "df62fd56";
+	
 	$db = "hackbean";
 	
 	$mysqli = new mysqli($host, $user, $pass, $db);
 
-
-	if ($mysqli->connect_errno) {
+	if ($mysqli->connect_errno) 
+	{
   	  echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
+	else
+	{
+		mysqli_query($mysqli, "INSERT INTO users (username, password) VALUES ('Justin', 'test')");
+	}
+	
 	echo $mysqli->host_info . "\n";
+	
+	
+	
+	
 ?>
 
