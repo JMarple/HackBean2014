e2e57ea67b912c1060e92747b86cbd0f1cf4d808
Marc Rudkowski <coolrudski@gmail.com>
2014-02-08 01:22:39 -0500

    maps

--- search.php
+++ search.php
@@ -6,19 +6,19 @@
         <title> Inbetween App</title>
     </head>
         
-    
+    <link rel="stylesheet" type="text/css" href="styles.css" />
     
     
         <body> 
         
-        <form action="welcome.php" method="post">
-        Name: <input type="text" Where You Are="Where you are"><br>
+        <form action="map.php" method="post">
+        Where You are: <input type="text" Where You Are="Where you are"><br>
         </form>
             
         <br>    
             
-        <form action="welcome.php" method="post">
-        Name: <input type="text" Where They Are="Where they are"><br>
+        <form action="map.php" method="post">
+        Where you are: <input type="text" Where They Are="Where they are"><br>
         </form>   
         
         
