8a61a333a862e27de65880d07db1de98e9a8ad55
Justin Marple <Jmarple@umass.edu>
2014-02-08 03:17:42 -0500

    TravelAPI + data output

--- find.php
+++ find.php
@@ -3,14 +3,23 @@
 	require_once "Requests/Requests/library/Requests.php";
 	Requests::register_autoloader();
 	
-	$request = Requests::get('http://api.tripadvisor.com/api/partner/1.0/search/boston+restaurants?key=92C34F58BB4F4E03894F5D171B79857E');
+	//Get Request
+	$request = Requests::get('http://api.tripadvisor.com/api/partner/1.0/map/42.34,-72.08?key=92C34F58BB4F4E03894F5D171B79857E&limit=50');
 	
-	//var_dump($request);
+	//Convert to array
 	$obj = json_decode($request->body, true);
 	
-	foreach($obj as $value)
+	$data;
+	
+	//Go through each object and get data
+	foreach($obj['data'] as $value)
 	{
-		echo $value[0]['location_string'];
-		echo "<br/>";
+		//var_dump($value);
+		echo $value['name'];
+		echo "<br/>Latitude:";
+		echo $value['latitude'];
+		echo "<br/>Longitude";
+		echo $value['longitude'];
+		echo "<br/><br/>";
 	}
 ?>
