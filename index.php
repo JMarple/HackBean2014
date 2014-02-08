df73f814d791652d11832a654f445af2607f21df
Justin Marple <Jmarple@umass.edu>
2014-02-08 01:29:02 -0500

    Login Start (note insecure)

--- index.php
+++ index.php
@@ -1,35 +1,53 @@
 <?php 
 	
 	session_start();
-	
-	if(isset($_SESSION['login']))
-	{
-		//Terribly insecure way to log in, to be changed
-		$user = mysql_real_escape_string($_POST['username']);
-		$password = mysql_real_escape_string($_POST['password']);
-		$result = mysql_query("SELECT * FROM users WHERE username = $user");
-		
-		$row = mysql_fetch_assoc($result);
-		
-		
-		
-	}
-	
-    echo "another test";
-    
-	//$host = "localhost";	
-	//$user = "root";	
-	//$pass = "";
 
 	$host = "us-cdbr-east-05.cleardb.net";
 	$user = "b85ad415edfa4d";
 	$pass = "df62fd56";
-		
+	
 	$db = "hackbean";
-		
+	
 	mysql_connect($host, $user, $pass);
 	mysql_select_db($db);
 	
-	mysql_query("INSERT INTO `heroku_807bde1acfd096e`.`hackbean` (`username`, `password`) VALUES ('Justin', 'test2')");
+	/* LOGIN */
+	if(isset($_POST['login']))
+	{
+		//Terribly insecure way to log in, to be changed
+		$user = mysql_real_escape_string($_POST['username']);
+		$password = mysql_real_escape_string($_POST['password']);
+		
+		if($result = mysql_query("SELECT * FROM `heroku_807bde1acfd096e`.`hackbean` WHERE `username` = '$user'"))
+		{		
+			$row = mysql_fetch_assoc($result);
+			
+			//If password matches then go for it
+			if(strcmp($row['password'], $password) == 0)
+			{
+				echo "Yes";
+			}
+		}	
+		else
+		{
+			echo mysql_error();
+		}
+	}
+ 	
+	//mysql_query("INSERT INTO `heroku_807bde1acfd096e`.`hackbean` (`username`, `password`) VALUES ('Justin', 'test2')");
 ?>
 
+<html>
+	<head>
+		<title>InBetween Home</title>
+	</head>
+	
+	<body>
+		<form action="index.php" method="post">
+			Username: <input type="text" name="username"/><br>
+			Password: <input type="password" name="password"/>
+			<input type="hidden" name="login"/>
+			<input type="submit"/>
+		</form>
+	</body>
+</html>
