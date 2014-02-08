27a51457a00125c2ba402bcfb50ec75827788c67
Justin Marple <Jmarple@umass.edu>
2014-02-08 00:50:58 -0500

    multi-page fix

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php 
 	
+	session_start();
+	
+	if(isset($_SESSION['login']))
+	{
+		//Terribly insecure way to log in, to be changed
+		$user = mysql_real_escape_string($_POST['username']);
+		$password = mysql_real_escape_string($_POST['password']);
+		$result = mysql_query("SELECT * FROM users WHERE username = $user");
+		
+		$row = mysql_fetch_assoc($result);
+		
+		
+		
+	}
+	
     echo "another test";
     
 	//$host = "localhost";	
 	//$user = "root";	
 	//$pass = "";
-	
 
 	$host = "us-cdbr-east-05.cleardb.net";
 	$user = "b85ad415edfa4d";
