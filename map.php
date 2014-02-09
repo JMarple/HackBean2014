c980962bb0a2880ac2ba68332f0c68f62dee9aaf
Justin Marple <Jmarple@umass.edu>
2014-02-08 21:16:27 -0500

    All Fixed Up

--- map.php
+++ map.php
@@ -13,8 +13,6 @@
 	mysql_select_db($db);
 	$data;
 	
-	echo "USERID: " . $userid;
-	
 	if($result = mysql_query("SELECT * FROM `heroku_807bde1acfd096e`.`group` WHERE `ID`='$userid'"))
 	{		
 		if(mysql_num_rows($result) > 0)
